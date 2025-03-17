f5e6215139f89dec2c826591360e2cd5b2264a98
mikeneko1224 <amiba725@gmail.com>
2025-03-17 23:08:12 +0900

    add Host screen

--- src/host/App.js
+++ src/host/App.js
@@ -5,18 +5,20 @@ import { useNavigate } from "react-router-dom";
 function App() {
   const navigate = useNavigate();
   const nameForm = () => {
-    navigate("/question");
+    navigate("/room");
   };
 
   return (
     <div className="App">
       <Header />
-      <div className="input_space">
+      <div className="children">
         <div className="text_input">
           <div>名前を入力してね！</div>
           <input type="text" name="text" value="例：田中 花子" />
         </div>
-        <button onClick={nameForm}>決定！！</button>
+        <button className="decide_button" onClick={nameForm}>
+          決定！！
+        </button>
       </div>
     </div>
   );
